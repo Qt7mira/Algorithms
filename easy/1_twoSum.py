be7565491057a83c1fc241ce21151bd8f316f4f5
qt7mira <771307676@qq.com>
2017-12-27 11:07:34 +0800

    P1

---
+++ easy/1_twoSum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        nums_dic = dict()
+        for index, value in enumerate(nums):
+            sub = target - value
+            if sub in nums_dic.keys():
+                return [nums_dic[sub], index]
+            else:
+                nums_dic[value] = index
+        return "no matched nums"
