46f2635492007e2e7841f6f354b101b82d5a2d3e
QT7mira <771307676@qq.com>
2019-10-10 18:07:58 +0800

    203

---
+++ leetcode_python/203.remove-linked-list-elements.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode.cn id=203 lang=python3
+#
+# [203] 移除链表元素
+#
+# https://leetcode-cn.com/problems/remove-linked-list-elements/description/
+#
+# algorithms
+# Easy (42.14%)
+# Likes:    296
+# Dislikes: 0
+# Total Accepted:    40.9K
+# Total Submissions: 96.3K
+# Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+#
+# 删除链表中等于给定值 val 的所有节点。
+#
+# 示例:
+#
+# 输入: 1->2->6->3->4->5->6, val = 6
+# 输出: 1->2->3->4->5
+#
+#
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode(0)
+        curr = dummy
+
+        while head:
+            if head.val != val:
+                curr.next = ListNode(head.val)
+                curr = curr.next
+            head = head.next
+
+        return dummy.next
+
+# @lc code=end
+
