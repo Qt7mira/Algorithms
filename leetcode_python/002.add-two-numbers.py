8d37844ab0b7277ab604388f58fe5af02534a5d0
QT7mira <771307676@qq.com>
2019-12-18 16:56:47 +0800

    002 003 添加说明

--- leetcode_python/002.add-two-numbers.py
+++ leetcode_python/002.add-two-numbers.py
@@ -1,34 +1,68 @@
+#
+# @lc app=leetcode id=2 lang=python3
+#
+# [2] Add Two Numbers
+#
+# https://leetcode.com/problems/add-two-numbers/description/
+#
+# algorithms
+# Medium (31.35%)
+# Likes:    5469
+# Dislikes: 1405
+# Total Accepted:    924.2K
+# Total Submissions: 2.9M
+# Testcase Example:  '[2,4,3]\n[5,6,4]'
+#
+# You are given two non-empty linked lists representing two non-negative
+# integers. The digits are stored in reverse order and each of their nodes
+# contain a single digit. Add the two numbers and return it as a linked list.
+#
+# You may assume the two numbers do not contain any leading zero, except the
+# number 0 itself.
+#
+# Example:
+#
+#
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# Explanation: 342 + 465 = 807.
+#
+#
+#
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        dummyHead = ListNode(0)
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+        result = ListNode(0)
+
         p = l1
         q = l2
-        curr = dummyHead
+        current = result
         carry = 0
+
         while p is not None or q is not None:
+
             x = p.val if p is not None else 0
             y = q.val if q is not None else 0
-            sum = carry + x + y
+
+            sum = x + y + carry
             carry = sum // 10
-            curr.next = ListNode(sum % 10)
-            curr = curr.next
+            sum = sum % 10
+
+            current.next = ListNode(sum)
+            current = current.next
+
             if p is not None:
                 p = p.next
             if q is not None:
                 q = q.next
 
         if carry > 0:
-            curr.next = ListNode(carry)
-        return dummyHead.next
+            current.next = ListNode(carry)
+
+        return result.next
--- leetcode_python/003.longest-substring-without-repeating-characters.py
+++ leetcode_python/003.longest-substring-without-repeating-characters.py
@@ -1,22 +1,52 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+#
+# [3] 无重复字符的最长子串
+#
+# https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
+#
+# algorithms
+# Medium (30.92%)
+# Likes:    2444
+# Dislikes: 0
+# Total Accepted:    221.9K
+# Total Submissions: 705.1K
+# Testcase Example:  '"abcabcbb"'
+#
+# 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+#
+# 示例 1:
+#
+# 输入: "abcabcbb"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+#
+#
+# 示例 2:
+#
+# 输入: "bbbbb"
+# 输出: 1
+# 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+#
+#
+# 示例 3:
+#
+# 输入: "pwwkew"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+# 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+#
+#
+#
 class Solution:
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-
-        start = 0
-        ans = 0
+    def lengthOfLongestSubstring(self, s: str) -> int:
         d = {}
+        start = 0
+        res = 0
 
         for index, value in enumerate(s):
             if value in d:
-                start = max(start, d[value] + 1)
+                start = max(start, d.get(value) + 1)
+            res = max(res, index - start + 1)
             d[value] = index
-            ans = max(ans, index - start + 1)
-            print(start, d, ans)
-        return ans
-
-
-sol = Solution()
-print(sol.lengthOfLongestSubstring("abcabcbb"))
+        return res
